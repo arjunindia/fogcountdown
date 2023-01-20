3c18795d1c1a5ec0798d39674c04653c7bde94c5
arjunindia <arjun8107@gmail.com>
2023-01-20 22:43:37 +0530

    The fog is coming

--- main.js
+++ main.js
@@ -4,25 +4,31 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 // in the format hh:mm:ss
 const date = dayjs("2023-01-21 02:30:00+0300");
 const audio = document.querySelector("#audio");
-
+ 
 const timer = setInterval(() => {
   const now = dayjs();
   const diff = date.diff(now, "second");
-  const hours = Math.floor(diff / 3600).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-  const minutes = Math.floor((diff % 3600) / 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-  const seconds = Math.floor(diff % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+  const hours = Math.floor(diff / 3600).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  const minutes = Math.floor((diff % 3600) / 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  const seconds = Math.floor(diff % 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
   const formatted = `${hours}:${minutes}:${seconds}`;
 
   if (diff <= 0) {
     clearInterval(timer);
-  }else{
+  } else {
     //slow down the audio
-    audio.playbackRate = 0.935;
+    audio.playbackRate = 1;
     audio.play();
   }
   //dom manipulation
   document.querySelector("#timer").innerHTML = formatted;
-  
-
-
-},1000);
+}, 1000);
